4d4fb2dcaedf47e256aed602591fc1f0ca863cdb
Mitke <miodrag.mitic@gmail.com>
2022-09-02 09:28:46 +0200

    lista

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import { useState } from "react";
 
 function App() {
-  const [title, setTitle] = useState("Zdravo Svete!");
-  const promeniNaslov = () => {
-    setTitle("NASLOV PROMENJEN!!!");
-  }
-
+  const [product, setProducts] = useState([
+    {id: 1, title: 'Product 1', price: 899},
+    {id: 2, title: 'Product 2', price: 982},
+    {id: 3, title: 'Product 3', price: 322},
+    {id: 4, title: 'Product 4', price: 763},
+    {id: 5, title: 'Product 5', price: 389}
+  ]);
+  
   return (
     <div>
-      <h1>{ title }</h1>
-      <button onClick={ promeniNaslov }>promeni naslov</button>
+      <ul>
+        {product.map((product) => (
+          <li key={product.id}> { product.title } - { product.price }</li>
+        ))}
+      </ul>
     </div>
   );
 }
